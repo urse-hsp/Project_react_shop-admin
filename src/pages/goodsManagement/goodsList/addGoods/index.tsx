a4fa096356f3ad3a540309788fb3c552f6cbb089
侯少平 <hsp_email@163.com>
2020-08-05 18:00:22 +0800

    商品列表添加进行

--- src/pages/goodsManagement/goodsList/addGoods/index.tsx
+++ src/pages/goodsManagement/goodsList/addGoods/index.tsx
@@ -1,17 +1,86 @@
-import React, { useState } from 'react'
-import { Card, Alert, Steps, Tabs } from 'antd'
+import React, { useState, useEffect } from 'react'
+import { Card, Alert, Steps, Tabs, Form, Input, InputNumber, Cascader, Checkbox, Upload, message, Button } from 'antd'
+import { UploadOutlined } from '@ant-design/icons'
+import { getGoodsClassifyList, getGoodsClassifyparameter } from './service'
 import styles from './styles.less'
 
 const { Step } = Steps
 const { TabPane } = Tabs
 
-const AddGoods: React.FC<{}> = () => {
-  const [current, setCurrent] = useState(0)
+const layout = {
+  labelCol: { span: 24 },
+  wrapperCol: { span: 24 },
+}
 
-  function callback(key) {
-    console.log(key)
-    setCurrent(key)
+const addGoodsRules = {
+  goods_name: [{ required: true, message: '请输入名称!' }],
+  goods_price: [{ required: true, message: '请输入价格!' }],
+  goods_weight: [{ required: true, message: '请输入重量' }],
+  goods_number: [{ required: true, message: '请输入数量' }],
+  goods_cat: [{ required: true, message: '请选择分类' }],
+}
+
+const props = {
+  name: 'file',
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  headers: {
+    authorization: 'authorization-text',
+  },
+}
+
+const AddGoods: React.FC<{}> = () => {
+  const [form] = Form.useForm()
+  const [current, setCurrent] = useState(0) // 标签页和时间轴 的选中
+  const [classifyDataList, setclassifyDataList] = useState([]) // 商品分类
+  const [actionGoodscat, setActionGoodscat] = useState([]) // 商品分类的默认选中
+  const [manyData, setmanyData] = useState([]) // many的参数
+  const [onlyData, setonlyData] = useState([]) // only的参数
+
+  function callback(key: any) {
+    form
+      .validateFields()
+      .then(async (values) => {
+        setCurrent(key)
+        if (key === '1') {
+          const { data, meta } = await getGoodsClassifyparameter({ id: values.goods_cat[2], sel: 'many' })
+          if (meta.status !== 200) return message.error(meta.msg)
+          data.forEach((item: any) => {
+            const Obj = item
+            Obj.attrs = Obj.attr_vals.split(' ')
+          })
+          setmanyData(data)
+        } else if (key === '2') {
+          const { data, meta } = await getGoodsClassifyparameter({ id: values.goods_cat[2], sel: 'only' })
+          console.log(data)
+          if (meta.status !== 200) return message.error(meta.msg)
+          setonlyData(data)
+        }
+        return true
+      })
+      .catch(() => {
+        message.warning('请输入基本信息!')
+      })
   }
+
+  const getDataList = async () => {
+    const { data, meta } = await getGoodsClassifyList()
+    if (meta.status !== 200) {
+      return message.error(meta.msg)
+    }
+    setclassifyDataList(data)
+    return true
+  }
+
+  const onChange = (value: any) => {
+    if (value.length < 3) {
+      form.setFieldsValue({ goods_cat: '' })
+    }
+  }
+
+  useEffect(() => {
+    getDataList()
+  }, [])
+
   return (
     <Card>
       <Alert message="添加商品信息" type="warning" showIcon />
@@ -23,23 +92,69 @@ const AddGoods: React.FC<{}> = () => {
         <Step title="商品内容" />
         <Step title="完成" />
       </Steps>
-      <Tabs defaultActiveKey={current} onChange={callback} tabPosition="left">
-        <TabPane tab="基本信息" key="1">
-          Content of Tab Pane 1
-        </TabPane>
-        <TabPane tab="商品参数" key="2">
-          Content of Tab Pane 2
-        </TabPane>
-        <TabPane tab="商品属性" key="3">
-          Content of Tab Pane 3
-        </TabPane>
-        <TabPane tab="商品图片" key="4">
-          Content of Tab Pane 4
-        </TabPane>
-        <TabPane tab="商品内容" key="5">
-          Content of Tab Pane 5
-        </TabPane>
-      </Tabs>
+      <Form {...layout} name="basic" initialValues={{ goods_cat: actionGoodscat }} form={form}>
+        <Tabs onChange={callback} tabPosition="left" activeKey={`${current}`}>
+          <TabPane tab="基本信息" key="0">
+            <Form.Item label="商品名称" name="goods_name" rules={addGoodsRules.goods_name}>
+              <Input size="large" />
+            </Form.Item>
+            <Form.Item label="商品价格" name="goods_price" rules={addGoodsRules.goods_price}>
+              <InputNumber min={0} max={100000} size="large" />
+            </Form.Item>
+            <Form.Item label="商品重量" name="goods_weight" rules={addGoodsRules.goods_weight}>
+              <Input size="large" />
+            </Form.Item>
+            <Form.Item label="商品数量" name="goods_number" rules={addGoodsRules.goods_number}>
+              <Input size="large" />
+            </Form.Item>
+            <Form.Item label="商品分类" name="goods_cat" rules={addGoodsRules.goods_cat}>
+              <Cascader
+                options={classifyDataList}
+                onChange={onChange}
+                size="large"
+                expandTrigger="hover"
+                style={{ width: '220px' }}
+                fieldNames={{ label: 'cat_name', value: 'cat_id', children: 'children' }}
+                popupPlacement="topLeft"
+              />
+            </Form.Item>
+          </TabPane>
+          <TabPane tab="商品参数" key="1">
+            {manyData &&
+              manyData.map((item: any) => {
+                return (
+                  <Form.Item label={item.attr_name} key={item.attr_id}>
+                    {item.attrs &&
+                      item.attrs.map((items: any) => {
+                        return <Checkbox key={items}>{items}</Checkbox>
+                      })}
+                  </Form.Item>
+                )
+              })}
+          </TabPane>
+          <TabPane tab="商品属性" key="2">
+            {onlyData &&
+              onlyData.map((item: any) => {
+                return (
+                  <Form.Item label={item.attr_name} key={item.attr_id}>
+                    <Input size="large" value={item.attr_vals} />
+                  </Form.Item>
+                )
+              })}
+          </TabPane>
+          <TabPane tab="商品图片" key="3">
+            <Upload {...props}>
+              <Button>
+                <UploadOutlined /> 上传图片
+              </Button>
+              只能上传jpg/png文件，且不超过500kb
+            </Upload>
+          </TabPane>
+          <TabPane tab="商品内容" key="4">
+            Content of Tab Pane 5
+          </TabPane>
+        </Tabs>
+      </Form>
     </Card>
   )
 }
---
+++ src/pages/goodsManagement/goodsList/addGoods/service.ts
@@ -0,0 +1,18 @@
+import request from 'umi-request'
+import { BASE_URL } from '@/utils/tool'
+// import { LoginParamsType } from './data'
+
+// 请求用户列表
+export async function getGoodsClassifyList() {
+  return request(`${BASE_URL}categories`, {
+    method: 'get',
+  })
+}
+
+// 根据 ID 查询参数
+export async function getGoodsClassifyparameter(params) {
+  return request(`${BASE_URL}categories/${params.id}/attributes`, {
+    method: 'get',
+    params: { sel: params.sel },
+  })
+}
